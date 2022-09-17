b83562f56d06b79fdaa184607eb9fb6be33a782a
baa-god <780810441@qq.com>
2022-09-17 14:03:58 +0800

    公开接口更改： eq.go 方法 `Not` 修改为 `Out`； eq.go 方法 `NonZero` 修改为 `NoZero`； eq.go 新增区间方法 `Between`；
    简化代码；
    `Value` 的 String() 方法现在返回具体的 sql 语句；因此可以从转化计算中更方便的获取 sql。
    
    版本需要更新到 1.19；
    添加注释；

--- chain.go
+++ chain.go
@@ -14,38 +14,38 @@ type Builder interface {
 type Map map[string]interface{}
 
 // ToSql 返回 sql 字符串
-func (b Map) ToSql() string { return "" }
+func (m Map) ToSql() string { return "" }
 
 // Cut 返回过滤后的 And 对象
-func (b Map) Cut() Map {
-	for k, v := range b {
+func (m Map) Cut() Map {
+	for k, v := range m {
 		mvv := reflect.ValueOf(v)                                       // map 中每个值的反射类型
 		if mvv.Kind() == reflect.Slice || mvv.Kind() == reflect.Array { // map 的值为切片或数组类型
 			for i := mvv.Len() - 1; i >= 0; i-- { // 正序删除 mvv 元素会出错
 				v = mvv.Index(i).Interface()
 				if val, ok := v.(Value); ok {
-					if val.isNot || val.isNonZero && reflect.ValueOf(val.value).IsZero() { // 排除该元素
+					if val.isOut || val.isNoZero && reflect.ValueOf(val.value).IsZero() { // 排除该元素
 						mvv = reflect.AppendSlice(mvv.Slice(0, i), mvv.Slice(i+1, mvv.Len())) // 移除第 i 个元素
 					}
 				}
 			}
 
 			if mvv.Len() == 0 {
-				delete(b, k)
+				delete(m, k)
 				continue
 			}
 
-			b[k] = mvv.Interface()
+			m[k] = mvv.Interface()
 		}
 
 		if val, ok := v.(Value); ok {
-			if val.isNot || val.isNonZero && reflect.ValueOf(val.value).IsZero() { // 排除该字段
-				delete(b, k)
+			if val.isOut || val.isNoZero && reflect.ValueOf(val.value).IsZero() { // 排除该字段
+				delete(m, k)
 			}
 		}
 	}
 
-	return b
+	return m
 }
 
 // ---- 等于 AND ----
@@ -72,8 +72,6 @@ func toSql(eq map[string]interface{}, chain string) string {
 
 	for k, v := range eq {
 		mvv := reflect.ValueOf(v)
-		_, isMark := v.(string) // 是否给值[v]加上单引号
-
 		if mvv.Kind() == reflect.Slice || mvv.Kind() == reflect.Array {
 			listSql := toInSql(mvv)
 			sql += fmt.Sprintf("%s %s %s ", k, listSql, chain)
@@ -82,12 +80,9 @@ func toSql(eq map[string]interface{}, chain string) string {
 
 		if val, ok := v.(Value); ok {
 			vv := reflect.ValueOf(val.value)
-			if val.isNot || val.isNonZero && vv.IsZero() { // 排除该字段
+			if val.isOut || val.isNoZero && vv.IsZero() { // 排除该字段
 				continue
 			}
-
-			v = val.value // 改变 v 的值以追加到 sql
-			isMark = vv.Type().Kind() == reflect.String && !val.isExpr
 
 			if val.isGt {
 				operator = ">"
@@ -99,10 +94,14 @@ func toSql(eq map[string]interface{}, chain string) string {
 				operator = "<="
 			} else if val.isNe {
 				operator = "!="
+			} else if val.isBetween {
+				operator = "BETWEEN"
+			} else {
+				operator = "="
 			}
 		}
 
-		if isMark {
+		if _, ok := v.(string); ok {
 			sql += fmt.Sprintf("%s %s '%v' %s ", k, operator, v, chain)
 		} else {
 			sql += fmt.Sprintf("%s %s %v %s ", k, operator, v, chain)
@@ -117,19 +116,15 @@ func toInSql(list reflect.Value) string {
 
 	for i := 0; i < list.Len(); i++ {
 		v := list.Index(i).Interface() // 列表中的每个值
-		_, isMark := v.(string)        // 是否给值[v]加上单引号
 
 		if val, ok := v.(Value); ok {
 			vv := reflect.ValueOf(val.value)
-			if val.isNot || val.isNonZero && vv.IsZero() { // 排除该元素
+			if val.isOut || val.isNoZero && vv.IsZero() { // 排除该元素
 				continue
 			}
-
-			v = val.value // 改变 v 的值以追加到 sql
-			isMark = vv.Type().Kind() == reflect.String && !val.isExpr
 		}
 
-		if isMark {
+		if _, ok := v.(string); ok {
 			sql += fmt.Sprintf("'%v', ", v)
 		} else {
 			sql += fmt.Sprintf("%v, ", v)
